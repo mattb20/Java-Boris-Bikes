83a166991e1f570085b94d8f8b7a0db5fc912b93
mattb20 <matt_burgoyne@live.co.uk>
2018-07-29 13:09:05 +0100

    created bike and docking station class files and relevent test files in test directory

---
+++ test/bikeTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.BeforeMethod;
+
+import static org.testng.Assert.*;
+
+public class bikeTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+}
---
+++ test/dockingstationTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.BeforeMethod;
+
+import static org.testng.Assert.*;
+
+public class dockingstationTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+}
